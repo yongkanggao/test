7cfab794f6223c0f9a6c9a145a05e97eee8eea3a
gaoyongkang <443929830@qq.com>
2016-08-22 18:02:44 +0800

    gogo

--- test/test1.py
+++ test/test1.py
@@ -1,18 +1,18 @@
 __author__ = 'yongkang'
 
+from collections import Counter
+
 some_list1 = [1,2,3,4]
-
 another_list1 = [x+1 for x in some_list1]
-
 print(another_list1)
 
 
 some_list2 = [1,2,3,4,5,6,7,8,9,10]
-
 another_list2 = [x for x in some_list2 if x%2 == 0]
-
 print(another_list2)
 
 some_list3 = {x:x%2 == 0 for x in range(1,11)}
-
 print(some_list3)
+
+c = Counter('hello world!')
+print(c)
